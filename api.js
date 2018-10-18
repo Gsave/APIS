45ce5714b7ef127bcbfe4079f42580ce9a571a21
Gianmaria Savegnago <gianmariasavegnago@MBPdiGianmaria.homenet.telecomitalia.it>
2018-10-18 09:15:54 +0200

    revisione errori file api e aggiunta package

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-const express = require(éxpress')
+const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 
@@ -8,4 +8,4 @@ app.get('/', (req,res) => res.send('Hello World!'))
 
 app.get('/courses', (req,res) => {res.json(courses_offered)})
 
-app.listen(PORT, () => console.log(Example app listening on port'+ PORT))
+app.listen(PORT, () => console.log('Example app listening on port'+ PORT))
